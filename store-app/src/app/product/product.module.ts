742153989da10c9e5fad22616ec5bcf8933a456c
kzarama <zaramaluna1999@hotmail.com>
2021-06-22 09:02:26 -0500

    contact modularized

--- store-app/src/app/product/product.module.ts
+++ store-app/src/app/product/product.module.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ProductComponent } from './components/product/product.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
